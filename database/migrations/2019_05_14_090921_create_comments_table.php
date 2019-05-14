de08a758756d08951a888b3461746c34cf4ce411
Vova Okhrimenko Odessa <tipodessa14@gmail.com>
2019-05-14 13:27:21 +0300

    Доработал миграцию комментариев

--- database/migrations/2019_05_14_090921_create_comments_table.php
+++ database/migrations/2019_05_14_090921_create_comments_table.php
@@ -17,7 +17,7 @@ class CreateCommentsTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
-            $table->string('slug');
+            $table->string('comments_text');
             $table->timestamps();
 
 
